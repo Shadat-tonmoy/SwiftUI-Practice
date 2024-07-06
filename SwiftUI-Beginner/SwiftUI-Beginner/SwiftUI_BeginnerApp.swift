d9a5a64754b66f593a2c736ecb0d33e7dc5d4527
shadat_tonmoy <shadat@kinetik.care>
2024-07-06 20:23:29 +0600

    first commit

---
+++ SwiftUI-Beginner/SwiftUI-Beginner/SwiftUI_BeginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BeginnerApp.swift
+//  SwiftUI-Beginner
+//
+//  Created by Shadat Mufakhkhar Tonmoy on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BeginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
