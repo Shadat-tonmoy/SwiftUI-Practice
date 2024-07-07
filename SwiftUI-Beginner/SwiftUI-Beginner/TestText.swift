67ffacd004e81d8eb8519097e84a3937953a2882
shadat_tonmoy <shadat@kinetik.care>
2024-07-07 09:43:37 +0600

    test

---
+++ SwiftUI-Beginner/SwiftUI-Beginner/TestText.swift
@@ -0,0 +1,18 @@
+//
+//  TestText.swift
+//  SwiftUI-Beginner
+//
+//  Created by Shadat Mufakhkhar Tonmoy on 6/7/24.
+//
+
+import SwiftUI
+
+struct TestText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestText()
+}
