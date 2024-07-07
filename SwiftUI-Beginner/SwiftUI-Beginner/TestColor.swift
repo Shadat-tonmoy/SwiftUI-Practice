10e87774e449816d0dcb63780113f43cde862d1d
shadat_tonmoy <shadat@kinetik.care>
2024-07-07 10:18:52 +0600

    completed swiftui color

---
+++ SwiftUI-Beginner/SwiftUI-Beginner/TestColor.swift
@@ -0,0 +1,18 @@
+//
+//  TestColor.swift
+//  SwiftUI-Beginner
+//
+//  Created by Shadat Mufakhkhar Tonmoy on 7/7/24.
+//
+
+import SwiftUI
+
+struct TestColor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestColor()
+}
