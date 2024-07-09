a9263a9aded8fe6aeb7b703437d875e822b2db87
shadat_tonmoy <shadat@kinetik.care>
2024-07-09 11:43:21 +0600

    completed overlay, background, shadow, stacks

---
+++ SwiftUI-Beginner/SwiftUI-Beginner/TestBackground.swift
@@ -0,0 +1,18 @@
+//
+//  TestBackground.swift
+//  SwiftUI-Beginner
+//
+//  Created by Shadat Mufakhkhar Tonmoy on 9/7/24.
+//
+
+import SwiftUI
+
+struct TestBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestBackground()
+}
---
+++ SwiftUI-Beginner/SwiftUI-Beginner/TestImage.swift
@@ -0,0 +1,18 @@
+//
+//  TestImage.swift
+//  SwiftUI-Beginner
+//
+//  Created by Shadat Mufakhkhar Tonmoy on 7/7/24.
+//
+
+import SwiftUI
+
+struct TestImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestImage()
+}
