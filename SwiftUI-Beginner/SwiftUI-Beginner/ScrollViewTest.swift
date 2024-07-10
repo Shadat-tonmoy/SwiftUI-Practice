ce745d8a60bd1f7c1bd80cee85bd960416a0812e
shadat_tonmoy <shadat@kinetik.care>
2024-07-10 10:26:27 +0600

    completed switfui scrollview with lazy v stack and lazy h stack

---
+++ SwiftUI-Beginner/SwiftUI-Beginner/ScrollViewTest.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollViewTest.swift
+//  SwiftUI-Beginner
+//
+//  Created by Shadat Mufakhkhar Tonmoy on 10/7/24.
+//
+
+import SwiftUI
+
+struct ScrollViewTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollViewTest()
+}
