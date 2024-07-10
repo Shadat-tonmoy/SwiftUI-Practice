35d2609594572277c61f77ed99b1f06ba786d198
shadat_tonmoy <shadat@kinetik.care>
2024-07-10 10:10:29 +0600

    completed switfui init, enum and foreach loop

---
+++ SwiftUI-Beginner/SwiftUI-Beginner/ForEachTest.swift
@@ -0,0 +1,18 @@
+//
+//  ForEachTest.swift
+//  SwiftUI-Beginner
+//
+//  Created by Shadat Mufakhkhar Tonmoy on 10/7/24.
+//
+
+import SwiftUI
+
+struct ForEachTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForEachTest()
+}
---
+++ SwiftUI-Beginner/SwiftUI-Beginner/InitializerTest.swift
@@ -0,0 +1,18 @@
+//
+//  InitializerTest.swift
+//  SwiftUI-Beginner
+//
+//  Created by Shadat Mufakhkhar Tonmoy on 10/7/24.
+//
+
+import SwiftUI
+
+struct InitializerTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InitializerTest()
+}
